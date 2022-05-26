1cd07a4bac9616866add59f47ae6c5dcc04f7993
Naiemisalm <nayemislam30964@gmail.com>
2022-05-26 13:54:35 +0600

    user add

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ async function run() {
         await client.connect();
         console.log('data connected')
         const toolsCllection = client.db('ElectricTools').collection('Tools')
+        const bookigCllection = client.db('ElectricTools').collection('bookings')
+        const userCllection = client.db('ElectricTools').collection('users')
         
         app.get('/tools', async(req, res)=>{
             const query = {};
@@ -28,12 +30,7 @@ async function run() {
             const tools = await cursor.toArray();
             res.send(tools)
         });
-        // app.get('/tools/:id', async(req, res)=>{
-        //     const id = req.params.id;
-        //     const query = {_id: ObjectId(id)};
-        //     const tools = await toolsCllection.findOne(query);
-        //     res.send(tools);
-        //   });
+        
         app.get('/tools/:id',async (req,res)=>{
             const id = req.params.id;
             console.log(id)
@@ -41,6 +38,26 @@ async function run() {
             const tools = await toolsCllection.findOne(query);
             res.send(tools);
         })
+        app.put('/user/:email', async(req,res)=>{
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email:email};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCllection.updateOne(filter, updateDoc, options);
+            // const token = jwt.sign({ email: email },'e122b7a6b8c5eecb94f993a4d63707b0664766c18669e123b1a6d4bf4d0799e8a13c6f97db3b45d5be5c07d13427db730c20ead62ca374f90daad7f765456545', { expiresIn: '1h' })
+            res.send( result);
+
+        })
+
+        app.get('/booking', async (req,res)=>{
+            const booking = req.body;
+            const result =await bookigCllection.insertOne(booking)
+            res.send(result)
+        })
+    
     } 
     finally {
 
